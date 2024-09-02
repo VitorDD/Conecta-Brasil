389491101c61aeed63a3a22412a7635f2d977fc2
Leonardo Perosa Zago <110700602+Leonard0Zago@users.noreply.github.com>
2024-09-02 20:21:31 -0300

    Add files via upload

---
+++ CONECTA BRASIL/CONECTA BRASIL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CONECTA_BRASIL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
